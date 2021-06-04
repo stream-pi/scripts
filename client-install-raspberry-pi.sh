cd2812c62e76da48f8de156db46cb4f923d5d1e7
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-06-04 23:45:58 +0530

    Update client-install-raspberry-pi.sh

--- client-install-raspberry-pi.sh
+++ client-install-raspberry-pi.sh
@@ -136,4 +136,5 @@ echo Stream-Pi Client is installed. However your Pi needs to be restarted
 echo After Restart, You may cd to $INSTALL_DIRECTORY$FOLDER_NAME 
 echo and run './run_console' or './run_desktop'
 read -n 1 -s -r -p $'Press any key to continue\n'
+
 sudo reboot
