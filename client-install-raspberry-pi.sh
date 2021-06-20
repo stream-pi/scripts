777ff0a426e0d450eb448c6a3d3e870fc81596c7
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-06-20 22:49:17 +0530

    Update client-install-raspberry-pi.sh

--- client-install-raspberry-pi.sh
+++ client-install-raspberry-pi.sh
@@ -154,7 +154,7 @@ echo Turning ON FAKE KMS Driver ...
 sudo sed "$CONFIG" -i -e "s/^dtoverlay=vc4-kms-v3d/#dtoverlay=vc4-kms-v3d/g"
 sudo sed "$CONFIG" -i -e "s/^#dtoverlay=vc4-fkms-v3d/dtoverlay=vc4-fkms-v3d/g"
 if ! sudo sed -n "/\[pi4\]/,/\[/ !p" "$CONFIG" | grep -q "^dtoverlay=vc4-fkms-v3d" ; then
-   sudo printf "[all]\ndtoverlay=vc4-fkms-v3d\n" >> "$CONFIG"
+   printf "[all]\ndtoverlay=vc4-fkms-v3d\n" | sudo tee "$CONFIG"
 fi
 
 
